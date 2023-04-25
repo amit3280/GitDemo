601f63ac97d92ec6f000aa28ecef2371418e502c
amit3280 <54405007+amit3280@users.noreply.github.com>
2023-04-25 23:59:29 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,6 +1,15 @@
+###Dev1 Code
 a = 33
 b = 43
 if b > a:
   print("b is greater than a")
 elif a == b:
   print("a and b are equal")
+
+###Dev2 Code
+i = 1
+while i < 6:
+  print(i)
+  if (i == 3):
+    break
+  i += 1
